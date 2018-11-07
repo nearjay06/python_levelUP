21ffc42f915d5ce803658ac7308995563b451ae7
nearjay06 <nearjoan2018@gmail.com>
2018-11-07 20:03:29 +0300

    add more tests

--- python_TDD/order_verify.py
+++ python_TDD/order_verify.py
@@ -9,3 +9,29 @@ def check_order_status(order_id):
    return True
  else:
    return False
+
+def add_new_order(order_id):
+  if order_id in pending_orders:
+    return "order_id exists"
+  else:
+    pending_orders.append(order_id)
+  return "order has been added"
+
+def order_marked_delivered (order_id):
+  if order_id in delivered_orders:
+    return 'order delivered'
+  else:
+    return 'order pending'
+
+def order_id_not_in_both_lists(order_id):
+  if order_id not in delivered_orders and pending_orders:
+    return True
+  else:
+    return False
+
+def orders_added_to_the_list(order_id):
+  for order_id in  pending_orders:
+    pending_orders.append(order_id)
+    return True
+  else:
+    return False
--- python_TDD/test_filename.py
+++ python_TDD/test_filename.py
@@ -1,15 +1,33 @@
 import unittest
-from order_verify import check_order_status
+from order_verify import check_order_status,add_new_order,order_marked_delivered,check_order_status
+from order_verify import order_id_not_in_both_lists,orders_added_to_the_list,pending_orders
 
 class TestOrder(unittest.TestCase):
     def test_order_status(self):
       self.assertEqual(check_order_status(2),True)
 
-    # def test_id_is_integer(self):
-    #   self.assertTrue(id_is_integer(2) )
+    def test_id_is_integer(self):
+      self.assertTrue(check_order_status(2),int )
+
+    def test_order_id_does_not_exist_in_both_lists(self):
+       self.assertEqual(order_id_not_in_both_lists(2),False)
+       
+
+    def test_if_orders_added_to_the_list(self):
+       self.assertEqual(orders_added_to_the_list(65),True)
+       self.assertIn(65,pending_orders)
 
 
+    def test_add_new_order(self):
+      self.assertTrue(add_new_order(3))
+      
+    def test_mark_order_delivered(self):
+       self.assertTrue(order_marked_delivered(2))
 
+    def check_order_id_status(self):
+      self.assertEqual(check_order_status(2), True)
+      
 
-    # def check_order_status(order_id):
-     # C:\Users\joan\Desktop\Python_OOP\python_oop\order_verify.py
+      
+      
+      unittest.main()
